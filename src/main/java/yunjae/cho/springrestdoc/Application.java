2245076ae9f1b7d6b7b70d19a55a2f92b6679bf0
comp1 <chotom73@gmail.com>
2018-09-20 08:00:20 +0900

    배포 방법을 jar -> war 변경 처리

--- src/main/java/yunjae/cho/springrestdoc/Application.java
+++ src/main/java/yunjae/cho/springrestdoc/Application.java
@@ -2,11 +2,25 @@ package yunjae.cho.springrestdoc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
+/*  jar 파일 실행 */
+//public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    /*  jar 파일 실행 *
+    /*public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }*/
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application .class);
+    }
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application .class, args);
     }
 }
